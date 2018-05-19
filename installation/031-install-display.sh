b030bba99c3dc77ec5229794b19f67deadb06222
Victor Cordes <victor.cordes@gmail.com>
2018-05-19 17:49:13 -0400

    lightdm install tweaks conf to include webkit greeter

--- installation/031-install-display.sh
+++ installation/031-install-display.sh
@@ -16,4 +16,21 @@ pacman -S lightdm-webkit-greeter --noconfirm --needed
 
 echo "---Webkit Greeter Installed---"
 
+sleep 3s
 
+# Replace greeter-session with corresponding greeter
+echo "---Modifying lightdm.conf---"
+
+sudo sed -i '/greeter.session/c\greeter-session=lightdm-webkit2-greeter' /etc/lightdm/lightdm.conf
+
+grep greeter-session /etc/lightdm/lightdm.conf
+
+echo "---Modification complete---"
+
+sleep 3s
+
+echo "---Enabling lightdm service---"
+
+systemctl enable lightdm.service
+
+echo "---Complete---"
