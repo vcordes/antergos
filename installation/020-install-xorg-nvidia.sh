3367f2817da7f8b94f6dd2baf7f81e16c20a0a45
Victor Cordes <victor.cordes@gmail.com>
2018-05-19 18:28:45 -0400

    spelling error 020 files

--- installation/020-install-xorg-nvidia.sh
+++ installation/020-install-xorg-nvidia.sh
@@ -6,7 +6,7 @@ set -e
 echo "---Xserver setup---"
 
 # --noconfirm don't ask --needed Do not reinstall the targets that are already up to date
-sudo pacman -S xorg-server xorg-apps xorg-xinit xorg-twm xterm --noconfirm --neded
+sudo pacman -S xorg-server xorg-apps xorg-xinit xorg-twm xterm --noconfirm --needed
 
 sudo pacman -S nvidia -video-intel --noconfirm --needed
 
