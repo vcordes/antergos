e2d581a764b9ecfe7d9c1a9174e2c3c4513b5cc7
Victor Cordes <victor.cordes@gmail.com>
2018-05-20 09:45:45 -0400

    added fonts to 040

--- installation/040-install-fonts.sh
+++ installation/040-install-fonts.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "---Installing fonts---"
 
-sudo pacman -S noto-fonts --noconfirm --needed
+sudo pacman -S noto-fonts ttf-roboto ttf-google-fonts-typewolf --noconfirm --needed
 
 echo "---Fonts installed---"
 
