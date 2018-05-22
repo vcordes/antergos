b6dffd0abb8910ad3ae8ad3a96958ab59f8193af
Victor Cordes <victor.cordes@gmail.com>
2018-05-22 19:11:44 -0400

    shadowfox

--- installation/060-install-ricing.sh
+++ installation/060-install-ricing.sh
@@ -4,6 +4,14 @@ set -e
 
 echo "---Installing Ricing Tools---"
 
-sudo pacman -S tint2 conky compton feh neofetch --noconfirm --needed
+sudo pacman -S tint2 conky compton feh neofetch rofi --noconfirm --needed
 
 echo "---Ricing Tools Installed---"
+
+sleep 3s
+
+echo "---Installing Firefox theme ShadowFox---"
+
+yaourt -S shadowfox-updater --noconfirm
+
+echo "---ShadowFox installed---"
