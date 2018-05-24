cedbe99f7d68ac3568020a1b10c48908633e76c4
Victor Cordes <victor.cordes@gmail.com>
2018-05-24 14:59:34 -0400

    tweak install files for errors or omissions (sardi)

--- installation/020-install-xorg-nvidia.sh
+++ installation/020-install-xorg-nvidia.sh
@@ -8,6 +8,6 @@ echo "---Xserver setup---"
 # --noconfirm don't ask --needed Do not reinstall the targets that are already up to date
 sudo pacman -S xorg-server xorg-apps xorg-xinit xorg-twm xterm --noconfirm --needed
 
-sudo pacman -S nvidia -video-intel --noconfirm --needed
+sudo pacman -S nvidia --noconfirm --needed
 
 echo "---xorg installed---"
--- installation/070-install-themes.sh
+++ installation/070-install-themes.sh
@@ -6,4 +6,5 @@ echo "---Installing Themes---"
 
 sudo pacman -S papirus-icon-theme arc-gtk-theme numix-frost-themes --noconfirm --needed
 
+yaourt -S sardi-icons --noconfirm --needed
 echo "---Theme Installed---"
